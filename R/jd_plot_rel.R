0cbcb173402b0825c3cee046c1e392cb8065a962
Jacky Dwiyanto <64482312+jdwiyanto@users.noreply.github.com>
2021-02-18 16:32:09 +0800

    Add files via upload

--- R/jd_plot_rel.R
+++ R/jd_plot_rel.R
@@ -58,29 +58,29 @@ jd_plot_rel <- function(ps, var, taxrank) {
 }
 
 #' @export
-jd_plot_phyla = function (ps, var) {
-  figlist = lapply(var, function(xx) {
-    sampledata = phyloseq::sample_data(ps)
-    sampledata = subset(sampledata, !is.na(sampledata[[xx]]))
-    ps2 = phyloseq::phyloseq(sampledata, otu_table(ps), tax_table(ps))
+jd_biome_rel = function(ps = ps.core.genus, testvar = testvar) {
 
-    ps2 = ps2 %>% phyloseq::tax_glom('Phylum')
-    keep = taxa_sums(ps2) > 0.1
-    ps2 = phyloseq::prune_taxa(keep, ps2) %>% phyloseq::psmelt() %>% dplyr::arrange('Phylum')
+  p.rel <- ps.genus.core %>% phyloseq::tax_glom('Phylum') %>%
+    phyloseq::transform_sample_counts(function(x) {x/sum(x)} ) %>%
+    phyloseq::psmelt() %>%
+    dplyr::arrange(Phylum)
 
-    figrel = ggplot2::ggplot(ps2, aes_string(x = xx, y = "Abundance", fill = 'Phylum')) +
-      ggplot2::geom_bar(stat = "identity", position = "fill") + ggplot2::labs(x = "group", y = "Relative abundance (%)") +
-      ggplot2::theme_bw() + ggplot2::theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.title = element_blank(), legend.position = "bottom") +
-      ggplot2::guides(fill = guide_legend(nrow = 5, byrow = T)) +
-      ggsci::scale_fill_d3(palette = "category20")
-    return(figrel)
+  test = p.rel[,c(testvar, 'Phylum', 'Abundance')]
+  #test = na.omit(test)
+
+  testdata = lapply(testvar, function(x) {
+    data = test[, c(x, 'Phylum', 'Abundance')]
+    data[[x]] = paste0(x, '_', tolower(data[[x]]))
+    names(data) = c('var', 'Phylum', 'Abundance')
+    return(data)
   })
-  if (var %>% length > 1) {
-    fig = ggpubr::ggarrange(plotlist = figlist, common.legend = T, labels = "auto")
-    return(fig)
-  }
-  else {
-    fig = ggpubr::ggarrange(plotlist = figlist, common.legend = T)
-    return(fig)
-  }
+  names(testdata) = testvar
+
+  testdata2 = bind_rows(testdata)
+  testdata2$Phylum = factor(testdata2$Phylum, levels = c('Proteobacteria', 'Desulfobacterota', 'Actinobacteriota', 'Bacteroidota', 'Firmicutes'))
+
+  fig = ggplot(testdata2, aes(x = var, fill = Phylum, y = Abundance)) + geom_bar(stat = 'identity', position = 'fill') +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    xlab(element_blank())
+  return(fig)
 }
